9c41034ce4bde237e6f3df246fb3674b7e53c3f6
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-05-13 00:26:39 +0100

    level 400

--- script.js
+++ script.js
@@ -1,10 +1,80 @@
+/* 
+
+DONE = Add a select input which allows you to choose which show you are interested in
+When a show is selected, your app should display 
+the episodes for that show as per the earlier levels of this challenge,
+except that it should first fetch the episode list from the API - see below
+You can get the list of shows by loading shows.js in your index.html and using the provided function: getAllShows()
+Ensure that your search and episode selector controls still work correctly when you switch shows.
+This show select must list shows in alphabetical order, case-insensitive.
+*/
+
 async function setup() {
   // const allEpisodes = getAllEpisodes();
   //fetch allEpisodes from API
   let allEpisodes = await fetchFromApi();
+  let allShows = getAllShows();
+  // let allEpisodes1 = await fetchFromApi1();
+  selectShow(allShows);
+
+  selectEpisode(allEpisodes);
+
   makePageForEpisodes(allEpisodes);
   makeLiveSearch(allEpisodes);
-  selectEpisode(allEpisodes);
+}
+
+function selectShow(shows) {
+  //preparing the elements for the container
+  let headerEl = document.getElementById("header-id");
+  let selectShowDivEl = document.createElement("div");
+  headerEl.appendChild(selectShowDivEl);
+  let selectTitleEl = document.createElement("h3");
+  selectTitleEl.textContent = "Select Show...";
+  selectShowDivEl.appendChild(selectTitleEl);
+  let selectEl = document.createElement("select");
+  selectShowDivEl.appendChild(selectEl);
+
+  for (let i = 0; i < 10; i++) {
+    // 1- create option 2-event listener 3- render the page out of for block
+
+    let option = document.createElement("option");
+    option.value = i;
+    option.text = shows[i].name;
+
+    selectEl.appendChild(option);
+  }
+
+  selectEl.addEventListener("click", function (event) {
+    console.log("You clicked option " + event.target.value);
+    //clear everything first
+    let contentEl = document.getElementById("content");
+    contentEl.innerHTML = "";
+    //then show new content here
+    let urlOfApi = shows[event.target.value]._links.self.href;
+    contentEl.innerHTML = urlOfApi; //just to test if its working
+    getEpisodesFromSelectedShow(urlOfApi); //will I need to pass the id as well
+  });
+}
+
+async function getEpisodesFromSelectedShow(url) {
+  //this function will fetch episodes based on the selected show
+  // how can we know which show? I think ID yes and episodes from href
+  let episodesFromShow = await fetchFromApi1(url);
+  console.log(episodesFromShow);
+  makePageForEpisodes(episodesFromShow);
+  makeLiveSearch(episodesFromShow);
+}
+
+//make it for shows then try to make it general
+async function fetchFromApi1(url) {
+  try {
+    const response = await fetch(url);
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
 async function fetchFromApi() {
@@ -21,14 +91,15 @@ async function fetchFromApi() {
 function selectEpisode(episodes) {
   //preparing the elements for the container
   let headerEl = document.getElementById("header-id");
-  let selectDivEl = document.createElement("div");
-  headerEl.appendChild(selectDivEl);
+  let selectShowDivEl = document.createElement("div");
+  selectShowDivEl.id = "select-div-el";
+  headerEl.appendChild(selectShowDivEl);
 
   let selectTitleEl = document.createElement("h3");
   selectTitleEl.textContent = "Select...";
-  selectDivEl.appendChild(selectTitleEl);
+  selectShowDivEl.appendChild(selectTitleEl);
   let selectEl = document.createElement("select");
-  selectDivEl.appendChild(selectEl);
+  selectShowDivEl.appendChild(selectEl);
 
   for (let i = 0; i < 10; i++) {
     // 1- create option 2-event listener 3- render the page out of for block
