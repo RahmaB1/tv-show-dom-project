ad2a8af3c698ef16b035b6683ffcce1e67dfb699
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-05-23 13:50:12 +0100

    updating level 500

--- script.js
+++ script.js
@@ -1,111 +1,99 @@
 async function setup() {
-  let allShows = getAllShows();
+  const allShows = await getAllShows();
   let urlForApi = `${allShows[0]._links.self.href}/episodes`;
-  let allEpisodes = await fetchFromApi1(urlForApi); //this line might be usfule when passing url to make it general ...let urlOfApi = `${shows[event.target.value]._links.self.href}/episodes`;
-  makePageForShows(allShows, allEpisodes);
-  // selectShow(allShows);
-  // selectEpisode(allEpisodes);
-  // makePageForEpisodes(allEpisodes);
-  // makeLiveSearch(allEpisodes);
+  let allEpisodes = await fetchFromApi1(urlForApi);
+  makePageForShows(allShows);
+  //  makePageForEpisodes(allEpisodes);
+  selectShow(allShows);
+  selectEpisode(allEpisodes);
+  // makeLiveSearch(allEpisodes); // need to make it work for both shows and episodes
+  navigateToShows(allShows);
+  searchInsideShows(allShows);
+}
+/*
+Provide a free-text show search 
+through show names, genres, and summary texts.
+*/
+
+function searchInsideShows(shows) {
+  let searchTitleEl = document.createElement("h4");
+  searchTitleEl.textContent = "Search....";
+  let searchBoxEl = document.createElement("input");
+  searchBoxEl.placeholder = "Search here ......";
+  let headerEl = document.getElementById("header-id");
+  headerEl.appendChild(searchTitleEl);
+  headerEl.appendChild(searchBoxEl);
+  let displayFilteredEpisodeNumberP = document.createElement("p");
+  headerEl.appendChild(displayFilteredEpisodeNumberP);
+
+  searchBoxEl.addEventListener("input", function () {
+    let searchedWords = searchBoxEl.value;
+    let showsAfterSearch = isShowIncludeWord(shows, searchedWords);
+
+    displayFilteredEpisodeNumberP.innerHTML = `Displaying ${showsAfterSearch.length}/${shows.length} episodes.`;
+    makePageForShows(showsAfterSearch);
+  });
 }
 
-function makePageForShows(shows, episodes) {
-  /*When your app starts, present a listing of all shows ("shows listing")
-For each show, you must display at least
-name, image, summary, genres, status, rating, and runtime.*/
+function navigateToShows(shows) {
+  let headerEl = document.getElementById("header-id");
+  let buttonEl = document.createElement("button");
+  buttonEl.innerHTML = "Back To All Shows..";
+  headerEl.appendChild(buttonEl);
 
+  buttonEl.addEventListener("click", () => {
+    makePageForShows(shows);
+  });
+}
+
+function makePageForShows(shows) {
   const mainContentElem = document.getElementById("content");
-
-  //clear everything before we start
-  mainContentElem.innerHTML = " ";
+  mainContentElem.innerHTML = "";
 
   for (let i = 0; i < shows.length; i++) {
     let sectionElm = document.createElement("section");
     mainContentElem.appendChild(sectionElm);
     sectionElm.className = "section-style";
-
     let episodeNameEl = document.createElement("h2");
     sectionElm.appendChild(episodeNameEl);
     episodeNameEl.className = "title-background";
-
     //will creat section for image and summary and inner suction then flex then row direciton
     let bottomSectionEl = document.createElement("section");
     bottomSectionEl.className = "bottom-section";
     sectionElm.appendChild(bottomSectionEl);
-
     let episodeImageEl = document.createElement("img");
     bottomSectionEl.appendChild(episodeImageEl);
-
     let summaryEl = document.createElement("p");
-    // summaryEl.style.padding="0rem"
-    // summaryEl.style.topMargin="50px";
     bottomSectionEl.appendChild(summaryEl);
-
     let innerSection = document.createElement("section");
     innerSection.className = "inner-section";
     bottomSectionEl.appendChild(innerSection);
-
-    // genres, status, rating, and runtime.*/
-
     let ratingEl = document.createElement("p");
     innerSection.appendChild(ratingEl);
-
     let genresEl = document.createElement("p");
     innerSection.appendChild(genresEl);
-
     let statusEl = document.createElement("p");
     innerSection.appendChild(statusEl);
-
     let runtimeEl = document.createElement("p");
     innerSection.appendChild(runtimeEl);
-
-    // let epiName = shows[i].name;
-    // let seasonNumber = shows[i].season;
-    // let episodeNumber = shows[i].number;
     episodeNameEl.textContent = shows[i].name;
     let fallbackImageUrl =
       "https://img.freepik.com/free-vector/gradient-no-photo-sign_23-2149263898.jpg?w=900&t=st=1684774054~exp=1684774654~hmac=f3127a286c175ffe2bb944611dec67f8ad4a14fd3acee082901e168742c6a008";
     episodeImageEl.src = shows[i].image?.medium ?? fallbackImageUrl;
-
     episodeImageEl.style.height = "10rem";
-
     summaryEl.innerHTML = shows[i].summary;
-
     ratingEl.innerHTML = `<strong>Rated: </strong> ${shows[i].rating.average}`;
     genresEl.innerHTML = `<strong>Genres: </strong> ${shows[i].genres}`;
     statusEl.innerHTML = `<strong>Status: </strong> ${shows[i].status}`;
     runtimeEl.innerHTML = `<strong>Runtime: </strong> ${shows[i].runtime}`;
 
-    /*
-
-    When a show name is clicked, your app should:
-    fetch and present episodes from that show (enabling episode search and selection as before)
-    hide the "shows listing" view.
-
-    */
     sectionElm.dataset.showId = i;
 
     sectionElm.addEventListener("click", function (event) {
       let showId = event.currentTarget.dataset.showId;
       let urlOfApi = `${shows[showId]._links.self.href}/episodes`;
-      console.log(urlOfApi);
       getEpisodesFromSelectedShow(urlOfApi);
     });
-
-    // when this show clicked I want to call makepages for episodes using the id maybe
-    // let showId= shows[i].id;
-    // let clickedShow = episodes.filter((episode) => {
-    //   return episode.id === shows[i].id;
-    // });
-    // console.log(clickedShow);
-    // let urlOfApi = `${shows[event.id]._links.self.href}/episodes`;
-    // getEpisodesFromSelectedShow(urlOfApi);
-    //console.log(`http://api.tvmaze.com/episodes/${shows[i].id}`);
-    // getEpisodesFromSelectedShow(`"http://api.tvmaze.com/episodes/"${shows[i].id}`);
-    // );
-
-    // I have problem here that I don't really know how to connect the show with the episode url maybe the word episode should come later
-    // });
   }
 }
 
@@ -130,17 +118,18 @@ function selectShow(shows) {
   selectEl.addEventListener("click", function (event) {
     console.log("You clicked option " + event.target.value);
     let urlOfApi = `${shows[event.target.value]._links.self.href}/episodes`;
-    getEpisodesFromSelectedShow(urlOfApi);
+    getEpisodesFromSelectedShow(urlOfApi, shows);
   });
 }
 
-async function getEpisodesFromSelectedShow(url) {
+async function getEpisodesFromSelectedShow(url, shows) {
   //this function will fetch episodes based on the selected show
   let episodesFromShow = await fetchFromApi1(url);
   console.log(episodesFromShow);
   makePageForEpisodes(episodesFromShow);
   selectEpisode(episodesFromShow);
   makeLiveSearch(episodesFromShow);
+  navigateToShows(shows);
 }
 
 //make it for shows then I'll try to make it general
@@ -201,34 +190,49 @@ function makeLiveSearch(allEpisodes) {
     let searchedWords = searchBoxEl.value;
     let episodesAfterSearch = isEpisodeIncludeWord(allEpisodes, searchedWords);
 
-    //to display number of results from the search
-    let lengthOfFilteredEpisodes =
-      episodesAfterSearch.length > 0 ? episodesAfterSearch.length : 0;
+    let lengthOfFilteredEpisodes = episodesAfterSearch.length;
     displayFilteredEpisodeNumberP.innerHTML = `Displaying ${lengthOfFilteredEpisodes}/${allEpisodes.length} episodes.`;
 
     makePageForEpisodes(episodesAfterSearch);
   });
+}
+function isShowIncludeWord(shows, word) {
+  let filteredShows = [];
+  //lowercase the genres array
 
-  function isEpisodeIncludeWord(allepisodes, word) {
-    let filteredEpisodes = [];
-
-    for (let episode of allepisodes) {
-      if (
-        episode.name.toLowerCase().includes(word.toLowerCase()) ||
-        episode.summary.toLowerCase().includes(word.toLowerCase())
-      ) {
-        filteredEpisodes.push(episode);
-      }
+  for (let show of shows) {
+    let loweredGenres = show.genres.map((element) => {
+      return element.toLowerCase();
+    });
+    if (
+      show.name.toLowerCase().includes(word.toLowerCase()) ||
+      show.summary.toLowerCase().includes(word.toLowerCase()) ||
+      loweredGenres.includes(word.toLowerCase())
+    ) {
+      filteredShows.push(show);
     }
-    return filteredEpisodes;
   }
+  return filteredShows;
+}
+
+function isEpisodeIncludeWord(allepisodes, word) {
+  let filteredEpisodes = [];
+
+  for (let episode of allepisodes) {
+    if (
+      episode.name.toLowerCase().includes(word.toLowerCase()) ||
+      episode.summary.toLowerCase().includes(word.toLowerCase())
+    ) {
+      filteredEpisodes.push(episode);
+    }
+  }
+  return filteredEpisodes;
 }
 
 function makePageForEpisodes(episodeList) {
   const mainContentElem = document.getElementById("content");
 
-  //clear everything before we start
-  mainContentElem.innerHTML = " ";
+  mainContentElem.innerHTML = " "; //clear everything
 
   for (let i = 0; i < episodeList.length; i++) {
     let sectionElm = document.createElement("section");
@@ -240,6 +244,8 @@ function makePageForEpisodes(episodeList) {
     episodeNameEl.className = "title-background";
 
     let episodeImageEl = document.createElement("img");
+    episodeImageEl.style.width = "10rem";
+
     sectionElm.appendChild(episodeImageEl);
 
     let summaryEl = document.createElement("p");
@@ -256,10 +262,7 @@ function makePageForEpisodes(episodeList) {
 
 window.onload = setup;
 
-/*
-two main issues here 
-solved = 1- duplication in header when selecting show =solved 
-2- the default episodes are gameOf thrones from level 100 
-
-
-*/
+//ideas:
+//make show images smaller
+//change color / theme
+//change episodes style as it was affected by show style
